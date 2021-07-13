7934dbe9b84b455ef4abc03c1ee53ab57635e9ba
Rauf Parchiev <rauf.parchiev@gmail.com>
2021-07-14 01:20:33 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,24 @@
+from telebot import *
+
+with open(".env", "r") as f:
+    TOKEN = f.read()
+print(TOKEN)
+bot = TeleBot(TOKEN)
+
+
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+    bot.reply_to(message, "Hello, send me a pic with dog or cat")
+
+
+@bot.message_handler(func=lambda message: True)
+def echo_all(message):
+    bot.reply_to(message, message.text)
+
+
+def main():
+    bot.polling()
+
+
+if __name__ == '__main__':
+    main()
